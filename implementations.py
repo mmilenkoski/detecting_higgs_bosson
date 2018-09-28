0f1da54c97c044dd0f8030231d99e92680cb6830
Samuel Bosch <samuelbosch@tsf-452-wpa-4-121.epfl.ch>
2018-09-28 18:11:01 +0200

    blablabala

--- implementations.py
+++ implementations.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 """some helper functions for project 1."""
 import numpy as np
+
+def compute_loss(y, tx, w):
+    return np.sum(((y-np.sum((tx*w),axis = 1))**2))/(2*len(y))
+
