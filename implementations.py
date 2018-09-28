85a394fb141d61d8b1699e684e113e6558c05042
Martin <martinmilenkoski53@gmail.com>
2018-09-28 18:28:54 +0200

    modify implementations.py

--- implementations.py
+++ implementations.py
@@ -3,5 +3,30 @@
 import numpy as np
 
 def compute_loss(y, tx, w):
-    return np.sum(((y-np.sum((tx*w),axis = 1))**2))/(2*len(y))
+    n = len(y)
+    e = y - np.dot(tx, w)
+    loss = np.sum(1/(2*n) * np.power(e, 2))
+    return loss
+	
+def compute_gradient(y, tx, w):
+    N = len(y)
+    e = y - np.dot(tx, w)
+    gradient = -1/N * np.dot(np.transpose(tx), e)
+    return gradient
+	
+def gradient_descent(y, tx, initial_w, max_iters, gamma):
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        gradient = compute_gradient(y, tx, w)
+        loss = compute_loss(y, tx, w)
+        w = w - gamma * gradient
+        ws.append(w)
+        losses.append(loss)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    return losses, ws
+
+
 
