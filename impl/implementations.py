24797f8170851c6d263de898de6301762954714a
martin <martinmilenkoski53@gmail.com>
2018-10-22 22:06:30 +0200

    change implementations.py

--- impl/implementations.py
+++ impl/implementations.py
@@ -32,7 +32,7 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     
     for n_iter in range(max_iters):
         # compute gradient
-        gradient, _ = compute_gradient(y, tx, w)
+        gradient, e = compute_gradient(y, tx, w)
         # calculate loss
         loss = compute_loss(y, tx, w)
         # update w
@@ -71,9 +71,9 @@ def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
     loss = -1
     
     for n_iter in range(max_iters):
-        for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size):
             # compute a stochastic gradient
-            gradient, _ = compute_gradient(y_batch, tx_batch, w)
+            gradient, e = compute_gradient(y_batch, tx_batch, w)
             # calculate loss
             loss = compute_loss(y, tx, w)
             # update w
@@ -99,9 +99,10 @@ def least_squares(y, tx):
         loss corresponding to the optimal weight vector
     """
     # calculate optimal weights
-    a = tx.T.dot(tx)
-    b = tx.T.dot(y)
-    w = np.linalg.solve(a, b)
+    tx_transpose = np.transpose(tx)
+    left = np.dot(tx_transpose, tx)
+    right = np.dot(tx_transpose, y)
+    w = np.linalg.solve(left, right)
     # calculate loss
     loss = compute_loss(y, tx, w)
     
@@ -131,13 +132,86 @@ def ridge_regression(y, tx, lambda_):
     # number of features
     D = tx.shape[1]
     # calculate optimal weights
-    a = tx.T.dot(tx) + 2 * N * lambda_ * np.identity(D)
-    b = tx.T.dot(y)
-    w = np.linalg.solve(a, b)
+    tx_transpose = np.transpose(tx)
+    lambda_prim = 2 * N * lambda_
+    left = np.dot(tx_transpose, tx) + lambda_prim  * np.eye(D)
+    right = np.dot(tx_transpose, y)
+    w = np.linalg.solve(left, right)
     # calculate loss
     loss = compute_loss(y, tx, w)
     
     return w, loss
+
+
+def compute_gradient(y, tx, w):
+    """Computes the gradient.
+    
+    Parameters
+    ----------
+    y: ndarray
+        1D array containing the correct labels of the training data. 
+    tx: ndarray
+        2D array containing the training data.
+    w: ndarray
+        1D array containing the weight vector
+     
+    Returns
+    -------
+    grad: ndarray
+        1D array containing the gradient
+    err: ndarray
+        1D array containing the errors for each training point
+    """
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
+def calculate_mse(e):
+    """Calculate the mse for vector e."""
+    n = e.shape[0]
+    mse = (1/2)*np.sum(e**2)/n
+    return mse
+
+
+def calculate_mae(e):
+    """Calculate the mae for vector e."""
+    n = e.shape[0]
+    mae = np.sum(np.abs(e))/n
+    return mae
+
+
+def compute_loss(y, tx, w):
+    """Calculate the loss.
+
+    You can calculate the loss using mse or mae.
+    """
+    e = y - np.dot(tx, w)
+    return calculate_mae(e)
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
 
 
 
